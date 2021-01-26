7a264de29cb1463c90f7112a47b66607a6a23ad1
gg134679852 <metal852794613@gmail.com>
2021-01-27 00:43:38 +0800

    update models.js

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,18 +3,22 @@ const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
   name: {
     type: String,
+    required: 'true'
   },
   category: {
     type: String,
+    required: 'true'
   },
   image: {
     type: String,
   },
   location: {
     type: String,
+    required: 'true'
   },
   phone: {
     type: String,
+    required: 'true'
   },
   google_map: {
     type: String,
